094b9252d529a49bc2892f7d936a28a36b206b2a
TheSeven66 <brandon.mas.seven@gmail.com>
2023-07-25 18:05:52 +0200

    footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
-import Translate from '@docusaurus/Translate';
+import styles from "./index.module.css";
+import Translate from "@docusaurus/Translate";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function HomepageHero() {
   return (
-    <div className='z-10 w-full bg-none'>
-      <div className='z-10 flex items-center w-full mx-auto'>
-        <div className="flex items-center w-full mx-auto bg-[#333333] bg-opacity-50">
-          <img src='/img/TVPaint002.png' className="object-cover w-full h-full mx-auto opacity-100 -z-10 aspect-video"/>
-          <div className="absolute left-0 right-0 mx-auto space-y-6 text-center w-fit">
-            <h1 className="z-20 text-white"><Translate>Documentation</Translate></h1>
-            <p className="z-20 text-white"><Translate>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus augue sit amet.</Translate></p>
-            <div className="z-20">
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/intro">
-                <Translate>Tutoriel Docusaurus - 5min ⏱️</Translate>
-              </Link>
+    <div className="relative flex flex-col flex-1 overflow-hidden">
+      <div className="relative h-[40vh] sm:h-[70vh]">
+        <img
+          className="object-cover object-center w-full h-full -z-10"
+          src="/img/tvpaint_hero.png"
+          alt=""
+          priority
+          fill
+        />
+        <div class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black h-3/4" />
+        <div class="absolute bottom-0 left-0 right-0 z-10 px-4 py-16 mx-auto sm:py-20">
+          <div class="max-w-3xl mx-auto space-y-8 text-center sm:space-y-16">
+            <div class="flex flex-col space-y-6 md:space-y-10">
+              <div class="flex flex-col space-y-4">
+                <h1 className="z-20 font-sans text-2xl font-black text-white">
+                  <Translate>Documentation</Translate>
+                </h1>
+                <p className="z-20 max-w-lg mx-auto text-lg text-center text-white">
+                  <Translate>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris finibus augue sit amet.
+                  </Translate>
+                </p>
+              </div>
+              <div class="flex justify-center max-w-lg flex-col mx-auto gap-y-4 gap-x-6 md:gap-x-16">
+                <Link
+                  className="hover:scale-[1.04] disabled:opacity-50 disabled:cursor-not-allowed z-10 w-full px-8 py-2 bg-white rounded-lg transition-all duration-300 ease-in-out border-[2px] border-white hover:bg-black hover:text-white hover:no-underline"
+                  to="/docs/intro"
+                >
+                  <Translate>Introduction</Translate>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
@@ -31,40 +49,13 @@ function HomepageHeader() {
   );
 }
 
-function HomepageHero() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <div className="relative py-[15.7rem]">
-      <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-end w-full h-full bg-black bg-opacity-30 mdx:opacity-100">
-        <img
-          className="object-cover w-full h-full -z-10"
-          src="/img/TVPaint002.png"
-          alt=""
-          priority
-          fill
-        />
-      </div>
-      <div className="flex flex-col items-center justify-center w-full max-w-screen-xl px-4 mx-auto space-y-6 mdx:space-y-10">
-        <h1 className="z-20 text-2xl font-black text-white font-title"><Translate>Documentation</Translate></h1>
-        <p className="z-20 max-w-lg text-lg text-center text-white font-title"><Translate>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus augue sit amet.</Translate></p>
-        <div className="z-20">
-          <Link
-            className="z-10 w-full px-8 py-2 bg-white rounded-lg transition-all duration-300 ease-in-out border-[2px] border-white hover:bg-black hover:text-white hover:no-underline"
-            to="/docs/intro">
-            <Translate>Introduction</Translate>
-          </Link>
-        </div>
-      </div>
-    </div>
-    );
-}
-
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />"
+    >
       <main>
         <HomepageHero />
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,59 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
-const tailwindcssForms = require('@tailwindcss/forms');
-const tailwindcssTypography = require('@tailwindcss/typography');
+const tailwindcssForms = require("@tailwindcss/forms");
+const tailwindcssTypography = require("@tailwindcss/typography");
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/context/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/context/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
-      xss: '390px',
-      xs: '480px',
-      sm: '640px',
-      md: '768px',
-      mdx: '960px',
-      header: '1012px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1560px',
-      '3xl': '1920px',
+      xss: "390px",
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      mdx: "960px",
+      header: "1012px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1560px",
+      "3xl": "1920px",
     },
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
     fontSize: {
-      sm: ['clamp(0.85rem, calc(0.85rem + 0.00vw), 0.85rem)', '1.1rem'],
-      md: ['clamp(0.9rem, calc(0.9rem + 0.00vw), 0.9rem)', '1.2rem'],
-      base: ['clamp(1.07rem, calc(1.07rem + 0.00vw), 1.07rem)', '1.6rem'],
-      lg: ['clamp(1.3rem, calc(1.3rem + 0vw), 1.3rem)', '2.2rem'],
-      '2lg': ['clamp(1.85rem, calc(1.85rem + 0vw), 1.85rem)', '2.75rem'],
-      xl: ['clamp(2.6rem, calc(2.6rem + 0.00vw), 2.6rem)', '3.25rem'],
-      '2xl': ['clamp(2.9rem, calc(2.9rem + 0.00vw), 2.9rem)', '3.75rem'],
-      '3xl': ['clamp(3.75rem, calc(3.75rem + 0.00vw), 3.75rem)', '5rem'],
-      '4xl': ['clamp(4.375rem, calc(4.375rem + 0.00vw), 4.375rem)', '5rem'],
-      '5xl': ['clamp(5.6rem, calc(5.6rem + 0vw), 5.6rem)', '5.5rem'],
+      sm: ["clamp(0.85rem, calc(0.85rem + 0.00vw), 0.85rem)", "1.1rem"],
+      md: ["clamp(0.9rem, calc(0.9rem + 0.00vw), 0.9rem)", "1.2rem"],
+      base: ["clamp(1.07rem, calc(1.07rem + 0.00vw), 1.07rem)", "1.6rem"],
+      lg: ["clamp(1.3rem, calc(1.3rem + 0vw), 1.3rem)", "2.2rem"],
+      "2lg": ["clamp(1.85rem, calc(1.85rem + 0vw), 1.85rem)", "2.75rem"],
+      xl: ["clamp(2.6rem, calc(2.6rem + 0.00vw), 2.6rem)", "3.25rem"],
+      "2xl": ["clamp(2.9rem, calc(2.9rem + 0.00vw), 2.9rem)", "3.75rem"],
+      "3xl": ["clamp(3.75rem, calc(3.75rem + 0.00vw), 3.75rem)", "5rem"],
+      "4xl": ["clamp(4.375rem, calc(4.375rem + 0.00vw), 4.375rem)", "5rem"],
+      "5xl": ["clamp(5.6rem, calc(5.6rem + 0vw), 5.6rem)", "5.5rem"],
     },
     extend: {
       fontFamily: {
-        sans: ['Lato', ...defaultTheme.fontFamily.sans],
-        title: ['PPAgrandir', ...defaultTheme.fontFamily.sans],
+        sans: ["PPAgrandir", ...defaultTheme.fontFamily.sans],
+        title: ["PPAgrandir", ...defaultTheme.fontFamily.sans],
       },
       height: {
-        header: '6rem',
-        breadcrumbs: '3rem',
+        header: "6rem",
+        breadcrumbs: "3rem",
       },
       colors: {
-        primary: '#3B2020',
-        secondary: '#DEA850',
-        tertiary: '#FFFDEF',
+        primary: "#3B2020",
+        secondary: "#DEA850",
+        tertiary: "#FFFDEF",
 
-        red: '#D53543',
-        grey: '#F8F8F8',
+        red: "#D53543",
+        grey: "#F8F8F8",
       },
       zIndex: {
         header: 100,
@@ -65,92 +65,92 @@ module.exports = {
       },
       keyframes: {
         grow: {
-          '0%, 100%': { transform: 'scale(1.1)' },
-          '50%': { transform: 'scale(1.05)' },
+          "0%, 100%": { transform: "scale(1.1)" },
+          "50%": { transform: "scale(1.05)" },
         },
       },
       animation: {
-        grow: 'grow 3s infinite',
+        grow: "grow 3s infinite",
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.primary'),
+            color: theme("colors.primary"),
             strong: {
-              color: theme('colors.primary'),
+              color: theme("colors.primary"),
             },
             p: {
-              fontSize: '16px',
-              marginTop: '0.75rem',
-              marginBottom: '0.75rem',
+              fontSize: "16px",
+              marginTop: "0.75rem",
+              marginBottom: "0.75rem",
             },
-            '> :first-child': {
-              marginTop: '0rem',
-              marginBottom: '0.75rem',
+            "> :first-child": {
+              marginTop: "0rem",
+              marginBottom: "0.75rem",
             },
-            '> :last-child': {
-              marginTop: '0.75rem',
-              marginBottom: '0rem',
+            "> :last-child": {
+              marginTop: "0.75rem",
+              marginBottom: "0rem",
             },
             a: {
-              color: theme('colors.red'),
-              '&:hover': {
-                color: theme('colors.red'),
+              color: theme("colors.red"),
+              "&:hover": {
+                color: theme("colors.red"),
               },
             },
             img: {
               margin: 0,
             },
             h1: {
-              fontSize: theme('fontSize.xl'),
-              marginTop: '0.5em',
-              marginBottom: '0',
-              color: theme('colors.primary'),
+              fontSize: theme("fontSize.xl"),
+              marginTop: "0.5em",
+              marginBottom: "0",
+              color: theme("colors.primary"),
             },
             h2: {
-              fontSize: '1.625rem',
-              marginTop: '2rem',
-              marginBottom: '1.5rem',
-              color: theme('colors.primary'),
+              fontSize: "1.625rem",
+              marginTop: "2rem",
+              marginBottom: "1.5rem",
+              color: theme("colors.primary"),
             },
             h3: {
-              fontSize: '1.25rem',
-              marginTop: '2rem',
-              marginBottom: '1rem',
-              color: theme('colors.primary'),
+              fontSize: "1.25rem",
+              marginTop: "2rem",
+              marginBottom: "1rem",
+              color: theme("colors.primary"),
             },
             h4: {
-              fontSize: theme('fontSize.base'),
-              marginTop: '0.5em',
-              marginBottom: '0',
-              color: theme('colors.primary'),
+              fontSize: theme("fontSize.base"),
+              marginTop: "0.5em",
+              marginBottom: "0",
+              color: theme("colors.primary"),
             },
             h5: {
-              fontSize: theme('fontSize.md'),
-              marginTop: '0.5em',
-              marginBottom: '0',
-              color: theme('colors.primary'),
+              fontSize: theme("fontSize.md"),
+              marginTop: "0.5em",
+              marginBottom: "0",
+              color: theme("colors.primary"),
             },
             h6: {
-              fontSize: theme('fontSize.md'),
-              marginTop: '0.5em',
-              marginBottom: '0',
-              color: theme('colors.primary'),
+              fontSize: theme("fontSize.md"),
+              marginTop: "0.5em",
+              marginBottom: "0",
+              color: theme("colors.primary"),
             },
             ul: {
-              marginTop: '0',
-              marginBottom: '0',
+              marginTop: "0",
+              marginBottom: "0",
             },
             ol: {
-              marginTop: '0',
-              marginBottom: '0',
+              marginTop: "0",
+              marginBottom: "0",
             },
             li: {
-              fontSize: '16px',
-              marginTop: '0',
-              marginBottom: '0.75rem',
-              '&::marker': {
-                color: theme('colors.primary'),
+              fontSize: "16px",
+              marginTop: "0",
+              marginBottom: "0.75rem",
+              "&::marker": {
+                color: theme("colors.primary"),
               },
             },
           },
@@ -158,8 +158,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [
-    tailwindcssForms,
-    tailwindcssTypography,
-  ],
+  plugins: [tailwindcssForms, tailwindcssTypography],
 };
